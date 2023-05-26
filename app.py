6f66737ea63e1f6607960ce3cde85ae7769d53a3
Santhoshdeepsphere <120715127+Santhoshdeepsphere@users.noreply.github.com>
2023-05-26 19:04:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ with imcol3:
     st.write("")
 st.markdown("<p style='text-align: center; color: black; margin-top: -20px ; font-size:25px;'><span style='font-weight: bold'>ChatGPT Application: </span>Content Classifier</p>", unsafe_allow_html=True)
 # st.markdown("<div style='display: flex; margin-top: -45px ; justify-content: center;'><hr style='height: 2px; background-color: gray; width: 440px; border: none;'></div>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center; color: blue;margin-top: -15px ;font-size:20px;'><span style='font-weight: bold'></span>Classify Whether a Given Content is Written by Human or AI</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; color: blue;margin-top: -15px ;font-size:20px;'><span style='font-weight: bold'></span>Chatgpt prompt</p>", unsafe_allow_html=True)
 st.markdown("<hr style=height:2.5px;margin-top:0px;background-color:gray;>",unsafe_allow_html=True)
 #---------Side bar-------#
 
