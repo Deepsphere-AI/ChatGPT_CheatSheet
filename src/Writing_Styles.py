60a02a4dd06e9522b144f290df2da7e93b749c35
Santhoshdeepsphere <120715127+Santhoshdeepsphere@users.noreply.github.com>
2023-05-31 20:01:14 +0530

    Add files via upload

--- src/Writing_Styles.py
+++ src/Writing_Styles.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas as pd
+from src.ready_to_response import ready_response
 
 def writing():
     w1,col1,col2,w2=st.columns((1.5,2.5,4,.1))
@@ -18,7 +18,7 @@ def writing():
         with col2:
             st.write('# ')
             st.info("Please provide a detailed analysis of the economic impact of renewable energy adoption, considering factors such as job creation, environmental sustainability, and long-term cost savings.")
-    
+        ready_response()
     
     elif vAR_input == 'Informal':
         with col1:
@@ -28,7 +28,7 @@ def writing():
         with col2:
             st.write('# ')
             st.info("Hey ChatGPT, what's your favorite movie of all time? Let's chat about some cool flicks and share our thoughts on the finest films out there!")
-    
+        ready_response()
     
     elif vAR_input == 'Pesuasive':
         with col1:
@@ -38,7 +38,8 @@ def writing():
         with col2:
             st.write('# ')
             st.info("You are a passionate advocate for renewable energy. Engage in a conversation with ChatGPT and present a compelling argument for renewable energy sources. Highlight their environmental benefits, economic advantages, and long-term sustainability.")
-   
+        ready_response()
+
    
     elif vAR_input == 'Descriptive':
         with col1:
@@ -48,7 +49,8 @@ def writing():
         with col2:
             st.write('# ')
             st.info("Describe the mesmerizing beauty of a vibrant sunset over a serene ocean. The golden hues merge with the tranquil waves and paint the sky in a breathtaking display of nature's artistry.")
-    
+        ready_response()
+
     
     elif vAR_input == 'Humoros':
         with col1:
@@ -58,7 +60,8 @@ def writing():
         with col2:
             st.write('# ')
             st.info("Tell me a joke that will make me laugh and brighten my day.")
-   
+        ready_response()
+
    
     elif vAR_input == 'Narrative':
         with col1:
@@ -68,7 +71,8 @@ def writing():
         with col2:
             st.write('# ')
             st.info("Imagine you are embarking on a thrilling quest through a mystical forest, seeking a fabled artifact said to grant extraordinary powers. Share with me your journey, the challenges you face, the allies you meet, and the ultimate outcome of your adventure.")
-    
+        ready_response()
+
     
     elif vAR_input == 'Inspirational':
         with col1:
@@ -78,7 +82,8 @@ def writing():
         with col2:
             st.write('# ')
             st.info("Sharing some words of wisdom and inspiration that can motivate and uplift someone facing challenges. This will remind them of their inner strength and limitless possibilities.")
-   
+        ready_response()
+
    
     elif vAR_input == 'Confrontational':
         with col1:
@@ -88,3 +93,5 @@ def writing():
         with col2:
             st.write('# ')
             st.info("Present an argument challenging the effectiveness of renewable energy sources in mitigating climate change. This argument highlights potential drawbacks and limitations that often become unnoticed.")
+        ready_response()
+        
