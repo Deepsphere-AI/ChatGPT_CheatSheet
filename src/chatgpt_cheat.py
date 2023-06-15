d43d04e3325581fc1775763dd6f0ecb5a3742a17
Santhoshdeepsphere <120715127+Santhoshdeepsphere@users.noreply.github.com>
2023-06-15 19:51:44 +0530

    Add files via upload

--- src/chatgpt_cheat.py
+++ src/chatgpt_cheat.py
@@ -1,10 +1,12 @@
 import streamlit as st
-from src.learning_How_to_prompt import How_to_prompt
-from src.role_playing import role_playing
-from src.learning import learn
-from src.Writing_Styles import writing
+from src.Art_of_prompt import How_to_prompt
+from src.Content_Creation import content_Creation
+from src.Knowledge import learn
+from src.writing_formats import writing
 from src.jailbreak import jail_breake
 from src.Prompting_techniques import Prompt_technique
+from src.tone_modifier import Tone_Modifier
+from src.Playing_role import role_playing
 
 def Chatgpt_Cheat():
     w1,col1,col2,w2=st.columns((2,5.5,5.5,.5))
@@ -22,20 +24,19 @@ def Chatgpt_Cheat():
             st.write('# ')
             st.markdown("<p style='text-align: left; color: black; font-size:20px;'><span style='font-weight: bold'>Select Model Category</span></p>", unsafe_allow_html=True)
         with col2:
-            vAR_Category = ['Select','Jailbreak','Role Playing','Learning','Writing Styles','Prompting Techniques','Learning How to Prompt?']
+            vAR_Category = ['Select','Art of prompt','Playing a role','Knowledge','Writing formats','Content Creation','Tone Modifier','Prompting Techniques']
             vAR_input = st.selectbox(' ',vAR_Category)
-        if vAR_input =='Jailbreak':
-            with cc1:
-                st.write("# ")
-                # jail_breake(vAR_input_model)
-                st.warning("Currently Not Available")
-        elif vAR_input =='Role Playing':
+        if vAR_input =='Playing a role':
             role_playing(vAR_input_model)
-        elif vAR_input =='Learning':
+        elif vAR_input =='Knowledge':
             learn(vAR_input_model)
-        elif vAR_input =='Writing Styles':
+        elif vAR_input =='Writing formats':
             writing(vAR_input_model)
-        elif vAR_input =='Learning How to Prompt?':
+        elif vAR_input =='Art of prompt':
             How_to_prompt(vAR_input_model)
         elif vAR_input == 'Prompting Techniques':
             Prompt_technique(vAR_input_model)
+        elif vAR_input == 'Tone Modifier':
+            Tone_Modifier(vAR_input_model)
+        elif vAR_input == 'Content Creation':
+            content_Creation(vAR_input_model)
